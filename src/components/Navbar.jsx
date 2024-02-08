3dcc7e889fe6602f69db22b5c8f6e7d5a0dbb2be
yaw oppong <opp.mic@gmail.com>
2024-02-08 21:23:30 +0100

    css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,13 @@ const Navbar = () => {
   const { state, dispatch } = UseGlobalContext()
 
   return <nav>
-
     <div className="nav-center">
       <h2 className='title'>seaHorse</h2>
       <div className='shopping-bag'>
         <button className='bag-btn'><FaShoppingBag /></button>
         <button className="count-btn">{totalQuantity(state.items)}</button>
       </div>
-
     </div>
-
   </nav>
 }
 export default Navbar;
